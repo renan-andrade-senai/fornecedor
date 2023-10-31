59e0fda743777c6da611b98566951104a1aa3917
renan-andrade-senai <renan.andrade@edu.sc.senai.br>
2023-10-31 17:31:29 -0300

    primeiro commit

---
+++ src/test/java/br/com/alura/microservice/fornecedor/FornecedorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.microservice.fornecedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FornecedorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
